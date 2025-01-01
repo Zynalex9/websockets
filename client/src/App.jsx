fd55fd346d63a69403dca59b33a9e931480bbe19
Zynalex9 <alzn952@gmail.com>
2025-01-01 20:20:56 +0500

    A basic emit, on, broadcast setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,17 @@ import React, { useEffect } from "react";
 import { io } from "socket.io-client";
 const App = () => {
   const socket = io("localhost:3000");
-  useEffect(()=>{
-socket.on("connect",()=>{
-  console.log('connected')
-})
-  },[])
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+      socket.on("welcome", (s) => {
+        console.log(s);
+      });
+      socket.on("my-broadcast", (s) => {
+        console.log(s);
+      });
+    });
+  }, []);
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
--- server/server.js
+++ server/server.js
@@ -3,15 +3,20 @@ import express from "express";
 import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin: "http://localhost:5173",
-        methods:['GET','POST'],
-        credentials:true
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 io.on("connection", (socket) => {
   console.log("New socket-user connected", socket.id);
+  socket.emit("welcome", `welcome to the server ${socket.id}`);
+  socket.broadcast.emit(
+    "my-broadcast",
+    `A new user has joined the chat userId: ${socket.id}`
+  );
 });
 
 server.listen(3000, () => {
